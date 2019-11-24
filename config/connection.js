ca209d0abefc2012b32125bfe59e02cd711bae89
Maria may Roberts <mariamayr96@gmail.com>
2019-11-23 23:45:11 -0700

    Updated Files

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password29",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    var connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password29",
+        database: "burgers_db"
+    });
+};
 
 //make a connection
 connection.connect(function(err) {
